2c17e5387f06d167f421b199373ba46b2d5a8d08
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-08 23:26:12 -0400

    Created sample code

---
+++ package/module.py
@@ -0,0 +1,2 @@
+def example_function(name: str):
+    return f"Hello, my name is {name}"
---
+++ tests/test_module.py
@@ -0,0 +1,4 @@
+from package import module
+
+def test_example_function():
+    assert module.example_function("Jeremy") == "Hello, my name is Jeremy"
