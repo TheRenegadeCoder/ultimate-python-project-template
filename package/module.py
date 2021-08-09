7930855fad7e228709da66cdd9b442bc3568c370
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-09 13:26:50 -0400

    Added a logging example

--- package/module.py
+++ package/module.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 def example_module_function(name: str) -> str:
     """
     Generates a string including a name.
@@ -5,4 +9,5 @@ def example_module_function(name: str) -> str:
     :param str name: the name of someone
     :return: a string in the form 'Hello, my name is {name}'
     """
+    logger.debug(f"Constructing a string from the following input: {name}")
     return f"Hello, my name is {name}"
