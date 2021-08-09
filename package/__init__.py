0b7568f2599a03b4ebcf2b418b2bb1b93cf6c912
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-09 12:46:57 -0400

    Cleaned up docs

--- package/__init__.py
+++ package/__init__.py
@@ -4,6 +4,7 @@ def example_package_function(num: int) -> int:
     """
     Squares the input number and returns the result.
 
+    :param int num: a number
     :return: the square of the input
     """
     return num ** 2
--- package/module.py
+++ package/module.py
@@ -2,7 +2,7 @@ def example_module_function(name: str) -> str:
     """
     Generates a string including a name.
 
-    :param name: the name of someone
+    :param str name: the name of someone
     :return: a string in the form 'Hello, my name is {name}'
     """
     return f"Hello, my name is {name}"
