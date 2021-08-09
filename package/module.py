8b7b402e3051e1d781192417b3562d557f656d66
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-09 12:05:09 -0400

    Added docs to example_function

--- package/module.py
+++ package/module.py
@@ -1,2 +1,8 @@
 def example_function(name: str):
+    """
+    Generates a string with including a name.
+
+    :param name: the name of the user
+    :return: a string in the form 'Hello, my name is {name}'
+    """
     return f"Hello, my name is {name}"
